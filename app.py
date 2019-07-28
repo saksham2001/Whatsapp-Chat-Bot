13ac2ab8582b8ada789e36f3b87bdb80e4fe99b1
saksham2001 <44300901+saksham2001@users.noreply.github.com>
2019-07-28 18:46:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,8 @@ app = Flask(__name__)
 @app.route('/sms', methods=['POST'])
 def sms():
     number = request.form['From']
-    message_body = request.form['Body']
-
-    resp = twiml.Response()
-    resp.message('Hello {}, you said: {}'.format(number, message_body))
-    return str(resp)
+    message_body = request.form.get('Body')
+    return message_body
 
 if __name__ == '__main__':
     app.run()
