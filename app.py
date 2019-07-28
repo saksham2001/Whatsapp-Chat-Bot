422fbffaac9b19d41f723b87650ddd034a95d126
saksham2001 <44300901+saksham2001@users.noreply.github.com>
2019-07-28 19:08:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from twilio import twiml
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def main_page():
+    return "Hello Bruh!"
 @app.route('/sms', methods=['POST'])
 def sms():
     message_body = request.form.get('Body')
